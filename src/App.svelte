639340baacbcb9b1b0630b3155bc5f6db2f25d13
Paul Barry <pauljbarry3@gmail.com>
2023-06-04 11:14:44 -0700

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,22 @@
+<script>
+  let name = null
+  let nameEntered = false
+</script>
+
+<main>
+  <h1>My Game</h1>
+  {#if nameEntered && name}
+    <p>Hello, {name}!</p>
+    <button type="button" on:click={() => nameEntered = false}>
+      Change name
+    </button>
+  {:else}
+    <form on:submit|preventDefault={() => nameEntered = true}>
+      <label>
+        What is your name?
+        <input bind:value={name} />
+      </label>
+      <button type="submit">Submit</button>
+    </form>
+  {/if}
+</main>
